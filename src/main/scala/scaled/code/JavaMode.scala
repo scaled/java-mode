fb837dad856882c7aa4ee89bbbe24226d80d067f
Michael Bayne <mdb@samskivert.com>
2014-04-08 10:24:04 -0700

    Improvements!
    - moved effacers into config
    - improved color mappings
    - addded show-syntax fn to aid in debugging

--- src/main/scala/scaled/code/JavaMode.scala
+++ src/main/scala/scaled/code/JavaMode.scala
@@ -8,6 +8,41 @@ import scaled._
 import scaled.grammar.{Scoper, Selector, Span}
 import scaled.major.{CodeConfig, CodeMode}
 
+object JavaConfig extends ConfigDefs {
+  import EditorConfig._
+  import CodeConfig._
+
+  // map TextMate grammar scopes to Scaled style definitions
+  val colorizers = Map(
+    effacer("comment", commentStyle),
+    effacer("constant", constantStyle),
+    effacer("invalid", warnStyle),
+    effacer("keyword", keywordStyle),
+    effacer("storage.type.java", typeStyle), // TODO: handle match-specificity (drop .java)
+    effacer("storage.type.annotation", preprocessorStyle),
+    effacer("storage.modifier.java", keywordStyle),
+    effacer("storage.modifier.package", constantStyle),
+    effacer("storage.modifier.extends", keywordStyle),
+    effacer("storage.modifier.implements", keywordStyle),
+    effacer("storage.modifier.import", typeStyle),
+    effacer("storage.type.primitive", typeStyle),
+    effacer("entity.name.type.class", typeStyle),
+    effacer("entity.other.inherited-class", typeStyle),
+    effacer("entity.name.function.java", functionStyle),
+    effacer("variable.language", keywordStyle),
+    effacer("variable.parameter", variableStyle),
+    effacer("variable.other.type", variableStyle))
+
+  /** Compiles `selector` into a TextMate grammar selector and pairs it with a function that applies
+    * `cssClass` to buffer spans matched by the selector. */
+  def effacer (selector :String, cssClass :String) =
+    // TODO: first remove all code faces, then add the desired faces?
+    (Selector.parse(selector), (buf :Buffer, span :Span) => {
+      println(s"Applying $cssClass to $span")
+      buf.addStyle(cssClass, span)
+    })
+}
+
 @Major(name="java",
        tags=Array("code", "project", "java"),
        pats=Array(".*\\.java"),
@@ -16,30 +51,21 @@ import scaled.major.{CodeConfig, CodeMode}
 class JavaMode (editor :Editor, config :Config, view :RBufferView, disp :Dispatcher)
     extends CodeMode(editor, config, view, disp) {
 
-  // map TextMate grammar scopes to Scaled style definitions
-  import EditorConfig._
-  import CodeConfig._
-  val colorizers = Map(
-    Selector.parse("comment")  -> efface(commentStyle),
-    Selector.parse("constant") -> efface(constantStyle),
-    Selector.parse("invalid")  -> efface(warnStyle),
-    Selector.parse("keyword") -> efface(keywordStyle),
-    Selector.parse("storage.modifier") -> efface(keywordStyle),
-    Selector.parse("storage.type") -> efface(typeStyle),
-    Selector.parse("variable.parameter") -> efface(variableStyle),
-    Selector.parse("variable.language") -> efface(keywordStyle),
-    Selector.parse("variable.other.type") -> efface(variableStyle))
-
-  private def efface (cssClass :String) = (buf :Buffer, span :Span) => {
-    // TODO: first remove all code faces, then add the desired faces?
-    buf.addStyle(cssClass, span)
-  }
-
   // TEMP: for now use a TextMate grammar for code highlighting
   val scoper = new Scoper(Grammars.grammars, view.buffer)
-  scoper.apply(new Selector.Processor(colorizers))
+  scoper.apply(new Selector.Processor(JavaConfig.colorizers))
 
-  override def dispose () {} // TODO
+  override def configDefs = JavaConfig :: super.configDefs
+  override def keymap = super.keymap ++ Seq(
+    "M-A-p" -> "show-syntax" // TODO: also M-PI?
+  )
+  override def dispose () {} // TODO: remove all colorizations?
 
-  // TODO: things!
+  @Fn("Displays the TextMate syntax scopes at the point.")
+  def showSyntax () {
+    val ss = scoper.scopesAt(view.point())
+    view.popup() = Popup(if (ss.isEmpty) List("No scopes.") else ss, Popup.UpRight(view.point()))
+  }
+
+  // TODO: more things!
 }
