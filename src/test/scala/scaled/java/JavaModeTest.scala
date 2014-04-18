1eca413f6a48b328a63f1c9666c7effe18fbf38f
Michael Bayne <mdb@samskivert.com>
2014-04-18 14:52:34 -0700

    Test tweaks whilst debugging something.

--- src/test/scala/scaled/java/JavaModeTest.scala
+++ src/test/scala/scaled/java/JavaModeTest.scala
@@ -10,6 +10,7 @@ import org.junit._
 import scaled._
 import scaled.grammar._
 import scaled.impl.BufferImpl
+import scaled.major.CodeConfig
 
 class JavaModeTest {
 
@@ -18,15 +19,15 @@ class JavaModeTest {
     //      012345678901234567890123456789012345678901234567890123456789012345678901234567890123456
     /* 0*/ "package foo;",
     /* 1*/ "",
-    /* 2*/ "/** This is some test Java code that we'll use to test {@code Grammar} and specifically",
-    /* 3*/ " * the {@literal JavaDoc} grammar.",
-    /* 4*/ " * @see http://manual.macromates.com/en/language_grammars",
-    /* 5*/ " */",
-    /* 6*/ "public class Test extends Baffle {",
-    /* 7*/ "   /**",
-    /* 8*/ "    * A constructor, woo!",
-    /* 8*/ "    * @param foo for fooing.",
-    /*10*/ "    * @param bar for barring.",
+    /* 2*/ "/**",
+    /* 3*/ " * This is some test Java code that we'll use to test {@link Grammar} and specifically",
+    /* 4*/ " * the {@literal JavaDoc} grammar.",
+    /* 5*/ " * @see http://manual.macromates.com/en/language_grammars",
+    /* 6*/ " */",
+    /* 7*/ "public class Test extends Baffle {",
+    /* 8*/ "   /**",
+    /* 9*/ "    * A constructor, woo!",
+    /*10*/ "    * @param foo for fooing.",
     /*11*/ "    */",
     /*12*/ "   public Test () {}",
     /*13*/ "",
@@ -41,16 +42,19 @@ class JavaModeTest {
   def testBuffer (name :String, text :String) =
     BufferImpl(name, new File(name), new StringReader(text))
 
-  @Test def testParse () {
-    val html = getClass.getClassLoader.getResourceAsStream("HTML.tmLanguage")
-    val javaDoc = getClass.getClassLoader.getResourceAsStream("JavaDoc.tmLanguage")
-    val java = getClass.getClassLoader.getResourceAsStream("Java.tmLanguage")
-    // Grammar.parse(java).print(System.out)
-    val grammars = Seq(Grammar.parse(html), Grammar.parse(javaDoc), Grammar.parse(java))
+  val html = getClass.getClassLoader.getResourceAsStream("HTML.tmLanguage")
+  val javaDoc = getClass.getClassLoader.getResourceAsStream("JavaDoc.tmLanguage")
+  val java = getClass.getClassLoader.getResourceAsStream("Java.tmLanguage")
+  // Grammar.parse(java).print(System.out)
+  val grammars = Seq(Grammar.parse(html), Grammar.parse(javaDoc), Grammar.parse(java))
+
+  @Test def testStylesLink () {
     val buffer = testBuffer("Test.java", testJavaCode)
-    val scoper = new Scoper(grammars, buffer)
-    println(scoper.showMatchers(Set("#code", "#class")))
-    // TEMP: for now use a TextMate grammar for code highlighting
-    scoper.apply(new Selector.Processor(JavaConfig.effacers))
+    val scoper = new Scoper(grammars, buffer, List(new Selector.Processor(JavaConfig.effacers)))
+    // println(scoper.showMatchers(Set("#code", "#class")))
+    assertTrue("@link contents scoped as link",
+               scoper.scopesAt(Loc(3, 61)).contains("markup.underline.link.javadoc"))
+    assertEquals("@link contents styled as link",
+                 Styles(CodeConfig.preprocessorStyle), buffer.stylesAt(Loc(3, 61)))
   }
 }
