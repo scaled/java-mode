bd41d5ae49745d854e340298ce8d85c0f00e04fa
Michael Bayne <mdb@samskivert.com>
2014-11-26 21:01:35 -0800

    Use CodexMinorMode. DRY!

--- src/main/java/scaled/java/CodexJavaMode.java
+++ src/main/java/scaled/java/CodexJavaMode.java
@@ -15,22 +15,12 @@ import scaled.util.Errors;
 @Minor(name="codex-java",
        tags={ "java" },
        desc="A minor mode which enhances Java mode when a Codex is available.")
-public class CodexJavaMode extends MinorMode {
+public class CodexJavaMode extends CodexMinorMode {
 
-  public final ProjectSpace pspace;
-  public final Project project;
   public final JavaMode javaMode;
-
-  /** The most recent index for the buffer's source file, if any. */
-  public final OptValue<SourceIndex> index = OptValue.create();
 
   public CodexJavaMode (Env env, JavaMode javaMode) {
     super(env);
-    pspace = ProjectSpace.apply(window().workspace());
-    project = Project.apply(buffer());
-    note(project.indexer().indexed().onValue(Std.fnU(idx -> {
-      if (idx.store().equals(buffer().store())) index.update(idx);
-    })));
     this.javaMode = javaMode; // the major mode we work with
   }
 
@@ -43,48 +33,48 @@ public class CodexJavaMode extends MinorMode {
   @Fn("Queries for a method in the enclosing class and inserts an override definition for it. " +
       "If the chosen method is already overridden, this navigates to the method instead.")
   public void codexMethodOverride () {
-    if (!index.isDefined()) throw Errors.feedback("No Codex index available for this file.");
+    onEncloser(view().point().get().rowCol(), Std.fnU(encl -> {
+      if (encl.kind != Kind.TYPE) throw Errors.feedback(
+        "The point must be inside a class declaration.");
 
-    Buffer buffer = buffer();
-    Option<Def> enclO = index.get().encloser(buffer.offset(view().point().get().rowCol()));
-    if (!enclO.isDefined()) throw Errors.feedback("Could not find enclosing type.");
+      List<Def> meths = OO.resolveMethods(codex().stores(project()), encl, this::isOverridable);
+      // remove methods defined directly in encl
+      for (Iterator<Def> iter = meths.iterator(); iter.hasNext(); ) {
+        if (encl.id.equals(iter.next().outerId)) iter.remove();
+      }
 
-    Def encl = enclO.get();
-    if (encl.kind != Kind.TYPE) throw Errors.feedback(
-      "The point must be inside a class declaration.");
-
-    List<Def> meths = OO.resolveMethods(pspace.codex().stores(project), encl, this::isOverridable);
-    // remove methods defined directly in encl
-    for (Iterator<Def> iter = meths.iterator(); iter.hasNext(); ) {
-      if (encl.id.equals(iter.next().outerId)) iter.remove();
-    }
-
-    // wow, this is unfortunate
-    Completer<Def> comp = Completer.from(scaled.Iterable.view(meths),
-                                         Std.<Def,String>fn(def -> def.globalRef().id));
-    window().mini().read("Method:", "", methodHistory, comp).onSuccess(Std.fnU(meth -> {
-      Sig sig = meth.sig().orElseThrow(() -> Errors.feedback("Signature unavailable for " + meth));
-      long loc = view().point().get().atCol(0);
-
-      // if the previous line is non-blank, insert a blank line
-      long prev = view().point().get().prevStart();
-      if (buffer.line(prev).indexOf(Chars.isNotWhitespace()) != -1) loc = buffer.split(loc);
-
-      // now insert the method code
-      String pre = spaces(javaMode.computeIndent(Loc.row$extension(loc)));
-      loc = buffer.insertLine(loc, Line.apply(pre + "@Override"));
-      loc = buffer.insertLine(loc, Line.apply(pre + accessPrefix(meth.access) + sig.text + " {"));
-      String inpre = spaces(javaMode.computeIndent(Loc.row$extension(loc)));
-      long inside = Loc.atCol$extension(loc, inpre.length());
-      loc = buffer.insertLine(loc, Line.apply(inpre));
-      loc = buffer.insertLine(loc, Line.apply(pre + "}"));
-
-      // if the following line is non-blank, insert a blank line
-      if (buffer.line(loc).indexOf(Chars.isNotWhitespace()) != -1) loc = buffer.split(loc);
-
-      // leave the point inside the newly created method, ready for typing!
-      view().point().update(new Loc(inside));
+      // wow, this is unfortunate
+      Completer<Def> comp = Completer.from(scaled.Iterable.view(meths),
+                                           Std.<Def,String>fn(def -> def.globalRef().id));
+      window().mini().read("Method:", "", methodHistory, comp).onSuccess(Std.fnU(meth -> {
+        long loc = view().point().get().atCol(0);
+        insertMethod(loc, meth, true);
+      }));
     }));
+  }
+
+  private void insertMethod (long loc, Def meth, boolean leavePointInBody) {
+    Buffer buffer = buffer();
+    Sig sig = meth.sig().orElseThrow(() -> Errors.feedback("Signature unavailable for " + meth));
+
+    // if the previous line is non-blank, insert a blank line
+    long prev = view().point().get().prevStart();
+    if (buffer.line(prev).indexOf(Chars.isNotWhitespace()) != -1) loc = buffer.split(loc);
+
+    // now insert the method code
+    String pre = spaces(javaMode.computeIndent(Loc.row$extension(loc)));
+    loc = buffer.insertLine(loc, Line.apply(pre + "@Override"));
+    loc = buffer.insertLine(loc, Line.apply(pre + accessPrefix(meth.access) + sig.text + " {"));
+    String inpre = spaces(javaMode.computeIndent(Loc.row$extension(loc)));
+    long inside = Loc.atCol$extension(loc, inpre.length());
+    loc = buffer.insertLine(loc, Line.apply(inpre));
+    loc = buffer.insertLine(loc, Line.apply(pre + "}"));
+
+    // if the following line is non-blank, insert a blank line
+    if (buffer.line(loc).indexOf(Chars.isNotWhitespace()) != -1) loc = buffer.split(loc);
+
+    // leave the point inside the newly created method, ready for typing!
+    if (leavePointInBody) view().point().update(new Loc(inside));
   }
 
   private boolean isOverridable (Def def) {
