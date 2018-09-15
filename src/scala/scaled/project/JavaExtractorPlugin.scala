4adf385f9088572ec53926ab21abf8da06043a50
Michael Bayne <mdb@samskivert.com>
2018-09-15 08:29:41 -0700

    Re-enable JavaExtractor here for now.

--- src/scala/scaled/project/JavaExtractorPlugin.scala
+++ src/scala/scaled/project/JavaExtractorPlugin.scala
@@ -2,21 +2,21 @@
 // Scaled Java Project Support - helps Scaled Project framework grok Java.
 // http://github.com/scaled/java-project/blob/master/LICENSE
 
-// package scaled.project
+package scaled.project
 
-// import codex.extract.JavaExtractor
-// import scaled._
+import codex.extract.JavaExtractor
+import scaled._
 
-// @Plugin(tag="codex-extractor")
-// class JavaExtractorPlugin extends ExtractorPlugin {
+@Plugin(tag="codex-extractor")
+class JavaExtractorPlugin extends ExtractorPlugin {
 
-//   override val suffs = Set("java")
+  override val suffs = Set("java")
 
-//   override def extractor (project :Project, suff :String) =
-//     project.component[JavaComponent].map(java => new JavaExtractor() {
-//       override def classpath = java.buildClasspath
-//       override def log (msg :String) = project.log(msg)
-//       // if this is a JDK project, use summary mode to avoid grinding through the amazing four
-//       // hundred billion method bodies
-//     }.setSummaryMode(JDKRootPlugin.find(project.root.path).isDefined))
-// }
+  override def extractor (project :Project, suff :String) =
+    project.component[JavaComponent].map(java => new JavaExtractor() {
+      override def classpath = java.buildClasspath
+      override def log (msg :String) = project.log(msg)
+      // if this is a JDK project, use summary mode to avoid grinding through the amazing four
+      // hundred billion method bodies
+    }.setSummaryMode(JDKRootPlugin.find(project.root.path).isDefined))
+}
