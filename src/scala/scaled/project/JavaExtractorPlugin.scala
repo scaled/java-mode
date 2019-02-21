08a795731fa7c623e2b47e725050b1bc31fcbc36
Michael Bayne <mdb@samskivert.com>
2019-02-21 10:06:43 -0800

    We need to add the project classes now.
    JavaComponent no longer adds them to buildClasspath because we want control
    over when they are and are not included. Plus they are technically not part of
    a project's build classpath.

--- src/scala/scaled/project/JavaExtractorPlugin.scala
+++ src/scala/scaled/project/JavaExtractorPlugin.scala
@@ -14,7 +14,7 @@ class JavaExtractorPlugin extends ExtractorPlugin {
 
   override def extractor (project :Project, suff :String) =
     project.component[JavaComponent].map(java => new JavaExtractor() {
-      override def classpath = java.buildClasspath
+      override def classpath = java.classes ++ java.buildClasspath
       override def log (msg :String) = project.log(msg)
       // if this is a JDK project, use summary mode to avoid grinding through the amazing four
       // hundred billion method bodies
